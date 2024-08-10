4240dad436fd29b19f4b8aa07c06020cfab3354a
Kássio <sapokk.pinga@gmail.com>
2024-08-10 19:11:48 -0300

    att

---
+++ front-end/script/cadastro.js
@@ -0,0 +1,32 @@
+const URL_API = location.hostname === "localhost" || location.hostname === "127.0.0.1" ? 
+"http://localhost:3000" : "https//render..."
+
+
+function cadastro(){
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+
+  const user = {
+    name: nameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value
+  }
+
+  fetch(`${URL_API}/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      alert("Usuário cadastrado com sucesso!")
+    })
+    .catch(error => {
+      console.log(error)
+      alert("Erro ao cadastrar usuário!")
+    })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
+import {cors} from 'hono/cors'
 import usersRoute from './routes/users'
 
 const app = new Hono()
+
+app.use(cors())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
