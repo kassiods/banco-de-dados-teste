e0fbd7b643fb3e3f1285b66a58de70361b6f44d1
Kássio <sapokk.pinga@gmail.com>
2024-08-12 19:02:26 -0300

    d

--- front-end/script/cadastro.js
+++ front-end/script/cadastro.js
@@ -1,5 +1,5 @@
 const URL_API = location.hostname === "localhost" || location.hostname === "127.0.0.1" ? 
-"http://localhost:3000" : "https//render..."
+"http://localhost:3000" : "https://banco-de-dados-teste.onrender.com"
 
 
 function cadastro(){
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { Hono } from 'hono'
 import {cors} from 'hono/cors'
 import usersRoute from './routes/users'
+import loginRoute from './routes/login'
+import signupRoute from './routes/signup'
+import { jwt } from 'hono/jwt'
 
 const app = new Hono()
 
 app.use(cors())
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Opa eae!')
 })
 
+app.route("/login", loginRoute)
+app.route("/signup", signupRoute)
+app.use("/*", jwt({secret: Bun.env.JWT_SECRET as string}))
 app.route("/users", usersRoute)
 
 export default app
